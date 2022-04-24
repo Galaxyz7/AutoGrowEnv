451a408660b95ae40c9796963a4fe05c93dd7131
Galaxyz <97992906+Galaxyz7@users.noreply.github.com>
2022-04-24 16:20:51 -0500

    n/a

--- SensorInfoXS.ino
+++ SensorInfoXS.ino
@@ -1,3 +1,4 @@
+//https://www.youtube.com/watch?v=zF3dKpH1M9k
 //This is to get that data from the sensors 
 //configuration will very depending on breadboard setup.
 //May not work for specialized sensors may have special case uses 
